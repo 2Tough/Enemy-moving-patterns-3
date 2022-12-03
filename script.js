da41b69667161d53da080ad90d486220cc926dcf
2Tough <vainferro@gmail.com>
2022-12-04 01:03:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d')
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 20
+const numberOfEnemies = 100
 const enemiesArray = []
 let gameFrame = 0;
 
@@ -21,14 +21,14 @@ class Enemy {
         this.y = Math.random() * (canvas.height - this.height);
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
-        this.angle = 0;
-        this.angleSpeed = Math.random() * 2;
-        this.curve = Math.random() * 200;
+        this.angle = Math.random() * 500;
+        this.angleSpeed = Math.random() * 0.5 + 0.5;
+        this.curve = Math.random() * 200 + 50;
     }
     update() {
-        this.x = this.curve * Math.sin(this.angle * Math.PI/180) + (canvas.width /2 - this.width/2);
+        this.x =canvas.width/2 * Math.sin(this.angle * Math.PI/200) + (canvas.width /2 - this.width/2);
         //this.x -= this.speed;
-        this.y = this.curve * Math.cos(this.angle * Math.PI/180) + (canvas.height /2 - this.height/2);
+        this.y = canvas.height/2 * Math.cos(this.angle * Math.PI/270) + (canvas.height /2 - this.height/2);
         this.angle += this.angleSpeed
         //this.y += Math.random() * 5 - 2.5 ;
         if(this.x + this.width < 0) this.x = canvas.width;
